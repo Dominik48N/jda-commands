229c3a26bde2b66aceedea9272ccc3fbbf4979af
Dominik <admin@dominik48n.de>
2022-08-30 16:10:20 +0200

    Create command registry

---
+++ src/main/java/com/github/dominik48n/jdacommands/Command.java
@@ -0,0 +1,10 @@
+package com.github.dominik48n.jdacommands;
+
+public abstract class Command {
+
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/com/github/dominik48n/jdacommands/CommandRegistry.java
@@ -0,0 +1,17 @@
+package com.github.dominik48n.jdacommands;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandRegistry {
+
+    private final Map<String, Command> commands = new HashMap<>();
+
+    public void registerCommand(final Command command) {
+        this.commands.put(command.getName().toLowerCase(), command);
+    }
+
+    public void unregisterCommand(final String name) {
+        this.commands.remove(name);
+    }
+}
