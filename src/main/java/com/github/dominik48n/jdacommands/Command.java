af7a19cfc62693fe2722c3dfdfb30d9f9adce6a1
Dominik <yt.dominik48n@gmail.com>
2022-09-12 18:55:48 +0200

    Add copyright notice header

--- src/main/java/com/github/dominik48n/jdacommands/Command.java
+++ src/main/java/com/github/dominik48n/jdacommands/Command.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2022 Dominik48N
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.dominik48n.jdacommands;
 
 import com.google.common.base.Preconditions;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
